cc0f4f209feca275904555a96286adf55d5799d3
by09115 <by09115@outlook.kr>
2018-08-02 12:03:37 +0900

    몇몇 파일들을 수정했습니다.

--- Flask/app/api_1_0/UserLogin.py
+++ Flask/app/api_1_0/UserLogin.py
@@ -1,23 +1,76 @@
 from flask import Flask, request, jsonify
+from flask_restful import Api, reqparse
 
 from app.api_1_0.models.user import User
 
 app = Flask(__name__)
+api = Api(app)
 
 
-# def get_users_url():
+class GetData:
+
+    def get_users_url(self):
+        pass
+
+    def get_tour_name(self):
+        pass
+
+    def get_tour_image(self):
+        pass
+
+    def get_req_time(self):
+        pass
+
+    def get_theme(self):
+        pass
 
 
-class UserLogin():
+class UserLogin:
 
     def post(self):
-
         request_data = request.get_json()
         user = User(userId=request_data('userId'))
+        user.save()
 
-        outValue = {
-            'result': 'success',
-            'profileUrl': get_users_url(),
+        if request_data('userId') is None:
 
-        }
-        return jsonify(outValue)
+            out_value = {
+                'result': 'failure',
+                'profileUrl': GetData.get_users_url(),
+                'reqComment': {
+                    'tourName': GetData.get_tour_name(),
+                    'tourImage': GetData.get_tour_image()
+                },
+                'recommendSpot': {
+                    'reqTime': GetData.get_req_time(),
+                    'theme': GetData.get_theme()
+                },
+                'reqShare': {
+                    'tourName': GetData.get_tour_name()
+                }
+
+            }
+
+            return jsonify(out_value), 500
+
+        else:
+
+            out_value = {
+                'result': 'success',
+                'profileUrl': GetData.get_users_url(),
+                'reqComment': {
+                    'tourName': GetData.get_tour_name(),
+                    'tourImage': GetData.get_tour_image()
+                },
+                'recommendSpot': {
+                    'reqTime': GetData.get_req_time(),
+                    'theme': GetData.get_theme()
+                },
+                'reqShare': {
+                    'tourName': GetData.get_tour_name()
+                }
+            }
+            return jsonify(out_value), 200
+
+
+Api.add_resource(UserLogin, '/api/user/login')
