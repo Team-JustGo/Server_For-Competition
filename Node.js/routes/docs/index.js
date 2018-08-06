9f92fac07d18616434122a33732b131f132695a1
WonJunKim <kimjnsjwj@gmail.com>
2018-08-06 16:08:04 +0900

    add docs feature

---
+++ Node.js/routes/docs/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
+
+router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=/docs/api-spec.json'));
+router.use('/swagger-ui', express.static(pathToSwaggerUi));
+router.use('/api-spec.json', express.static('./api-spec.json'));
+
+module.exports = router;
