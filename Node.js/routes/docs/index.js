7d09539186ece2c6bad2db71e4bc217301ab8300
WonJunKim <kimjnsjwj@gmail.com>
2018-08-29 23:25:27 +0900

    delete public folder & change redirect url

--- Node.js/routes/docs/index.js
+++ Node.js/routes/docs/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
-router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=/docs/api-spec.json'));
+router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=api/docs/api-spec.json'));
 router.use('/swagger-ui', express.static(pathToSwaggerUi));
 router.use('/api-spec.json', express.static('../../public/api-spec.json'));
 
