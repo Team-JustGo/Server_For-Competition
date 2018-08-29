135d7f92a8002a14b28cae4ce75f6fa198b6d82c
WonJunKim <kimjnsjwj@gmail.com>
2018-08-29 23:38:16 +0900

    change json serving

--- Node.js/routes/docs/index.js
+++ Node.js/routes/docs/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
-router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=api/docs/api-spec.json'));
+router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=/api/docs/api-spec.json'));
 router.use('/swagger-ui', express.static(pathToSwaggerUi));
-router.use('/api-spec.json', express.static('./api-spec.json'));
+router.use('/api-spec.json', (req, res) => res.status(200).json(require('./api-spec.json')));
 
 module.exports = router;
