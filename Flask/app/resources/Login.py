ad456ac839dcb1dcf5eebe5134998d5161565996
by09115 <by09115@outlook.kr>
2018-09-14 23:16:28 +0900

    일부 파일들을 수정하였습니다.

--- Flask/app/resources/Login.py
+++ Flask/app/resources/Login.py
@@ -1,5 +1,5 @@
 from flask import request, Response, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from resources import connect
 
@@ -8,10 +8,16 @@ class SocialLogin(Resource):
 
 
     def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('userId',type=str,required=True)
+        parser.add_argument('name',type=str)
+        parser.add_argument('picture',type=str)
+        requests = parser.parse_args()
         user = connect.user
-        _userId = request.json['userId']
-        _name = request.json['name']
-        _picture = request.json['picture']
+        _userId = requests['userId']
+        _name = requests['name']
+        _picture = requests['picture']
         access_token = create_access_token(_userId)
         success_200 = {"result": "Success",
                        "jwt": access_token}
@@ -36,7 +42,7 @@ class SocialLogin(Resource):
 
             if (_userId not in multi_user_id) and _userId and _name and _picture:
                 connect.in_user.insert_one(second_situation)
-                return {"result": "Success"}, 200
+                return success_200, 200
 
             if (_userId not in multi_user_id) and _userId == None and _name == None and _picture == None:
                 return {"result": "I am a teapot"}, 418
