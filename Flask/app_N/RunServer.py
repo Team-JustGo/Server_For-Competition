07a1eab79729e78a4e360aa432818904acea5d9d
by09115 <by09115@outlook.kr>
2018-09-18 19:17:16 +0900

    Adolf

--- Flask/app_N/RunServer.py
+++ Flask/app_N/RunServer.py
@@ -1,25 +1,37 @@
-from flask import Flask, jsonify, request, url_for
+from flask import Flask, flash, redirect, jsonify, request, url_for, send_from_directory
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-from flask_restful import Api
+from flask_restful import Api, Resource
 from resources.Login import SocialLogin
 from resources.main import UserMain
 from resources.ProfileImage import ChangeProfileImage
 from resources.ProfileName import ChangeProfileName
-import resources.connect
+from resources.Upload import UploadImages
+from resources import connect
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = "INEEDMORESPEED"
+app.config['SECRET_KEY'] = UploadImages.UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 저장 가능한 파일의 최대 크기 = 16MiB
+
 jwt = JWTManager(app)
 api = Api(app)
+
 
 @app.route('/')
 def helloworld():
     return "<h1>Welcome to Just-go&#39;s second API Server!</h1><p>This API will give you many data! It so fun!</p><p>~~[This page is TEST]</p>"
 
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 api.add_resource(SocialLogin, '/api/user/login')
 api.add_resource(UserMain, '/api/user/main')
 api.add_resource(ChangeProfileImage, '/api/user/profile-image')
 api.add_resource(ChangeProfileName, '/api/user/profile-name')
+api.add_resource(UploadImages, '/uploads')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=7777)
+    app.run(host='0.0.0.0', debug=True, port=7777)
--- Flask/temp.py
+++ Flask/temp.py
@@ -1,26 +1,47 @@
-from flask import Flask
-from flask_restful import Api
-from pymongo import MongoClient
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
-uri = "mongodb://by09115:hunter5402@ds018508.mlab.com:18508/justgo"
-
-client = MongoClient(uri)
-db = client["justgo"]
-
-collection_contact = db.contact
-contact = collection_contact.find()
-
-collection_tourspot = db.tourspot
-tourspot = collection_tourspot.find()
-
-collection_user = db.user
-user = collection_user.find()
+UPLOAD_FOLDER = './FileHAM'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'bmp', 'gif', 'png'])
 
 app = Flask(__name__)
-api = Api(app)
+app.config['SECRET_KEY'] = 'ASDFGD'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-myID = "by0129115"
-TMI = db.user.find_one({"userId": myID})
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-if not TMI:
-    print("아몰라")
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method = post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
