9e4a35072c1bcf907a121dd5fc1ce7c35b79233b
by09115 <by09115@outlook.kr>
2018-09-18 19:53:56 +0900

    asfd

--- Flask/app_N/RunServer.py
+++ Flask/app_N/RunServer.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, flash, redirect, jsonify, request, url_for, send_from_directory
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_restful import Api, Resource
@@ -5,21 +6,49 @@ from resources.Login import SocialLogin
 from resources.main import UserMain
 from resources.ProfileImage import ChangeProfileImage
 from resources.ProfileName import ChangeProfileName
-from resources.Upload import UploadImages
 from resources import connect
+
+UPLOAD_FOLDER = './FileHAM'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'bmp', 'gif', 'png'])
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = "INEEDMORESPEED"
-app.config['SECRET_KEY'] = UploadImages.UPLOAD_FOLDER
+app.config['SECRET_KEY'] = "ASDFGH"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 저장 가능한 파일의 최대 크기 = 16MiB
 
 jwt = JWTManager(app)
 api = Api(app)
 
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
-def helloworld():
-    return "<h1>Welcome to Just-go&#39;s second API Server!</h1><p>This API will give you many data! It so fun!</p><p>~~[This page is TEST]</p>"
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file'
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method = post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 @app.route('/uploads/<filename>')
@@ -31,7 +60,6 @@ api.add_resource(SocialLogin, '/api/user/login')
 api.add_resource(UserMain, '/api/user/main')
 api.add_resource(ChangeProfileImage, '/api/user/profile-image')
 api.add_resource(ChangeProfileName, '/api/user/profile-name')
-api.add_resource(UploadImages, '/uploads')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=7777)
