fe5564c5d028641d0e0df3b723ac071653bb4837
WonJunKim <kimjnsjwj@gmail.com>
2018-09-20 19:14:39 +0900

    add module & promise & delete legacy model

--- Node.js/index.js
+++ Node.js/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config');
 
+mongoose.connect(config.DB_URL, { useNewUrlParser: true })
+  .then(() => console.log('connected to db'))
+  .catch(err => new Error(err));
 const app = express();
 
 app.use(logger('common', { stream: fs.createWriteStream('justgo_server.log', { flags: 'w' }) }))
