035e7d8df9d8a4021ae03a42f690b2306abf9134
WonJunKim <kimjnsjwj@gmail.com>
2018-09-20 19:07:10 +0900

    delete contact

--- Node.js/routes/index.js
+++ Node.js/routes/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 router.use('/docs', require('./docs'))
   .use('/travel', require('./travel'))
   .use('/user', require('./user'))
-  .use('/contact', require('./contact'))
   .get('/', (req, res) => {
     res.status(200).json({
       result: 'success',
