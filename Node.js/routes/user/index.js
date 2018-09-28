f5eb8c180ca547d4d6b4bd4ea4665cac27f076a7
WonJunKim <kimjnsjwj@gmail.com>
2018-09-29 03:30:33 +0900

    add user re-requests

--- Node.js/routes/user/index.js
+++ Node.js/routes/user/index.js
@@ -1,33 +1,78 @@
 const router = require('express').Router();
 const rp = require('request-promise');
+const multer = require('multer');
+
+const upload = multer();
 
 router.post('/login', (req, res) => {
-  const { userId, name, picture } = req.body;
+  const { userId, name } = req.body;
   rp({
-    url: 'http://localhost:5000/api/user/login',
+    url: 'http://ec2-52-79-240-33.ap-northeast-2.compute.amazonaws.com:7777/api/user/login',
     method: 'POST',
-    body: {
+    headers: { 'Content-Type': 'application/json' },
+    json: {
       userId,
       name,
-      picture,
+    },
+    resolveWithFullResponse: true,
+    simple: false,
+  }).then(data => res.status(data.statusCode).send(data.body));
+});
+
+router.put('/:id/profile-image', upload.array('picture'), (req, res) => {
+  const jwt = req.get('X-Access-Token');
+  const image = req.files['profile-image'];
+  rp({
+    url: 'http://ec2-52-79-240-33.ap-northeast-2.compute.amazonaws.com:7777/api/user/profile-image',
+    method: 'PUT',
+    headers: {
+      'X-Access-Token': `bearer ${jwt}`,
+    },
+    formData: {
+      'profile-image': image,
     },
     json: true,
     resolveWithFullResponse: true,
-  }).then((data) => {
-    res.status(200).send(data);
-  });
-});
-
-router.put('/:id/profile-image', (req, res) => {
-  // TODO: Reqeust 보내기
+    simple: false,
+  }).then(data => res.status(data.statusCode).send(data.body));
 });
 
 router.put('/:id/profile-name', (req, res) => {
-  // TODO: Reqeust 보내기
+  const jwt = req.get('X-Access-Token');
+  const { name } = req.body;
+  rp({
+    url: 'http://ec2-52-79-240-33.ap-northeast-2.compute.amazonaws.com:7777/api/user/login',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Access-Token': `bearer ${jwt}`,
+    },
+    body: {
+      name,
+    },
+    json: true,
+    resolveWithFullResponse: true,
+    simple: false,
+  }).then(data => res.status(data.statusCode).send(data.body));
 });
 
-router.post('/:id/tour-spot', (req, res) => {
-  // TODO: Reqeust 보내기
+router.post('/tour-spot', (req, res) => {
+  const jwt = req.get('X-Access-Token');
+  const { tourId } = req.body;
+  rp({
+    url: 'http://ec2-52-79-240-33.ap-northeast-2.compute.amazonaws.com:7777/api/user/login',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Access-Token': `bearer ${jwt}`,
+    },
+    body: {
+      tourId,
+    },
+    json: true,
+    resolveWithFullResponse: true,
+    simple: false,
+  }).then(data => res.status(data.statusCode).send(data.body));
 });
 
 module.exports = router;
