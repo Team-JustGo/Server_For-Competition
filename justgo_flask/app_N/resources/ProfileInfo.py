94f56523f76602f33b4327a8a5beda10a55a6e72
by09115 <by09115@outlook.kr>
2018-09-29 05:16:54 +0900

    strange errors...

--- justgo_flask/app_N/resources/ProfileInfo.py
+++ justgo_flask/app_N/resources/ProfileInfo.py
@@ -1,3 +1,5 @@
+# -*-coding: utf-8
+
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app_N.resources import connect
@@ -11,7 +13,8 @@ class ProfileInfo(Resource):
         user_list = connect.db.user.find_one({"userId": id_token})
 
         if user_list:
-            id_image = list(connect.db.user.find({"userId": "by09115"}, {"profileImage": 1, "profileName": 1, "_id": 0}))[0]
+            id_image = \
+            list(connect.db.user.find({"userId": "by09115"}, {"profileImage": 1, "profileName": 1, "_id": 0}))[0]
             return id_image
 
         elif not user_list:
